df8f31bade0e87cf7232d25f2afb21b95de30c8c
unknown <hhgg28170g@gmail.com>
2026-08-23 16:33:51 +0700

    A minor refactoring to improve code readability

--- src/codecs.rs
+++ src/codecs.rs
@@ -1,26 +1,28 @@
-use std::io::Error;
+use std::{
+    io::{Error, Read},
+    num::NonZeroUsize,
+};
 
-use bytes::{BytesMut};
+use bytes::BytesMut;
+use flate2::read::ZlibDecoder;
 use tokio_util::codec::Decoder;
 
+pub struct FragmentCodec {}
 
-pub struct FragmentCodec{}
-
-pub struct PacketFrame{
+pub struct PacketFrame {
     pub bytes: BytesMut,
-    pub offset: usize
+    pub offset: usize,
 }
 
 impl Decoder for FragmentCodec {
     type Item = PacketFrame;
     type Error = Error;
-    
+
     #[inline(always)]
     fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
         if src.len() < 2 {
             return Ok(None);
         }
-
 
         let mut position = 0;
 
@@ -34,44 +36,74 @@ impl Decoder for FragmentCodec {
 
             full_lenght |= ((current_byte & 0x7F) as u32) << (position * 7);
 
-
-            
             position += 1;
             if (current_byte & 0x80) == 0 {
                 break;
             }
 
             if position >= 5 {
-                return Err(Error::new(std::io::ErrorKind::FileTooLarge, "LEB128 so big"));
+                return Err(Error::new(
+                    std::io::ErrorKind::FileTooLarge,
+                    "LEB128 so big",
+                ));
             }
-
-
         }
 
         let total_len: usize = position + full_lenght as usize;
-
 
         if src.len() < total_len {
             return Ok(None);
         }
 
-
-        
-        let frame = PacketFrame {bytes: src.split_to(total_len as usize), offset: position};
+        let frame = PacketFrame {
+            bytes: src.split_to(total_len as usize),
+            offset: position,
+        };
 
         return Ok(Some(frame));
     }
 }
 
+#[inline(always)]
+pub fn get_compressed_length(data: &mut PacketFrame, is_compressed: bool) -> Option<NonZeroUsize> {
+    if is_compressed {
+        let mut position = 0;
+        let mut compressed_data: u32 = 0;
+        let bytes: &[u8] = data.bytes[data.offset..].as_ref();
 
+        loop {
+            let current_byte = bytes.get(position)?;
 
+            compressed_data |= ((current_byte & 0x7F) as u32) << (position * 7);
 
+            position += 1;
+            if (current_byte & 0x80) == 0 {
+                break;
+            }
 
+            if position >= 5 {
+                return None;
+            }
+        }
+        data.offset += position;
 
+        NonZeroUsize::new(compressed_data as usize)?;
+    }
+
+    None
+}
+
+#[inline(always)]
+pub fn uncompress(frame: &mut PacketFrame, buffer: &mut [u8]) -> Result<(), ()> {
+    ZlibDecoder::new(&frame.bytes[frame.offset..])
+        .read_exact(buffer)
+        .map_err(|_| ())?;
+    Ok(())
+}
 
 pub struct FinalPacket {
     pub id: u32,
-    pub offset: usize,
-    pub is_need_send: bool
+    pub length: usize,
+    pub is_compressed: bool,
+    pub is_need_send: bool,
 }
-
--- src/main.rs
+++ src/main.rs
@@ -1,302 +1,252 @@
-use std::io::{Error, Read};
-use std::num::{NonZeroU32, NonZeroUsize};
 use std::sync::atomic::{AtomicBool, AtomicU8, Ordering};
-use std::time::Duration;
 mod codecs;
 
+use bytes::BytesMut;
 
-use minstant::Instant;
-use bytes::{ BytesMut};
-use flate2::read::ZlibDecoder;
-use tokio::net::{TcpListener, TcpStream};
-use tokio::io::{AsyncWriteExt};
-use tokio_util::codec::{ FramedRead};
 use futures::StreamExt;
+use tokio::io::AsyncWriteExt;
+use tokio::net::{TcpListener, TcpStream};
+use tokio_util::codec::FramedRead;
 
-use crate::codecs::{FinalPacket, FragmentCodec, PacketFrame};
+use crate::codecs::*;
 
 static LISTENER_ADDRESS: &str = "127.0.0.1:25565";
 static CONNECTION_ADDRESS: &str = "";
 
-static GLOBAL_STATE: AtomicU8 = AtomicU8::new(3);
+static GLOBAL_STATE: AtomicU8 = AtomicU8::new(2);
 static IS_COMPRESSED: AtomicBool = AtomicBool::new(false);
 
 static BUFFER_SIZE: usize = 8 * 1024 * 1024;
 
-
-
 #[inline(always)]
-unsafe fn get_compressed_lenth(frame: &mut PacketFrame) -> Option<NonZeroUsize> {
-    if IS_COMPRESSED.load(Ordering::Relaxed) {
-
-        let mut position = 0;
-        let mut compressed_data: u32 = 0;
-        let bytes: &[u8] = frame.bytes[frame.offset..].as_ref();
-
-        unsafe {
-            loop {
-                
-                let current_byte = bytes.get_unchecked(position);
-
-                compressed_data |= ((current_byte & 0x7F) as u32) << (position * 7);
-
-
-                
-                position += 1;
-                if (current_byte & 0x80) == 0 {
-                    break;
-                }
-
-                if position >= 5 {
-                    return None;
-                }
-
-            }
-
-            frame.offset += position;
-
-            if compressed_data == 0 {
-                return None
-            } else {
-                return Some(NonZeroUsize::new_unchecked(compressed_data as usize));
-            }
+fn final_encode(
+    frame: &mut PacketFrame,
+    bytes: Option<&mut BytesMut>,
+    length: usize,
+    is_compressed: bool,
+) -> Result<FinalPacket, ()> {
+    let cur_bytes;
+    match bytes {
+        Some(v) => {
+            cur_bytes = &v[frame.offset..length];
+        }
+        None => {
+            cur_bytes = &frame.bytes[frame.offset..frame.bytes.len()];
         }
     }
-    return None;
-}
-
-
-
-
-
-
-
-#[inline(always)]
-fn uncompress(frame: &mut BytesMut, buffer: &mut [u8], offset: usize) -> Result<(),()> {
-    let mut zlib_decoder = ZlibDecoder::new(&frame[offset..]);
-
-    if zlib_decoder.read_exact(buffer).is_ok() {
-        return Ok(());
-    } else {
-        return Err(());
-    }
-}
-
-
-
-
-#[inline(always)]
-unsafe fn final_encode(offset: usize, bytes: &mut BytesMut, lenth: usize) -> Result<FinalPacket, ()> {
-    let cur_bytes = &bytes[offset..lenth];
 
     let mut position = 0;
     let mut id_data: u32 = 0;
 
-    unsafe {
-        loop {
-            let current_byte = cur_bytes.get_unchecked(position);
-            id_data |= ((current_byte & 0x7F) as u32) << (position * 7);
-            
-            position += 1;
-            if (current_byte & 0x80) == 0 {
-                break;
-            }
+    loop {
+        let current_byte = cur_bytes.get(position).ok_or(())?;
+        id_data |= ((current_byte & 0x7F) as u32) << (position * 7);
 
-            if position >= 5 {
-                return Err(());
-            }
+        position += 1;
+        if (current_byte & 0x80) == 0 {
+            break;
+        }
+
+        if position >= 5 {
+            return Err(());
         }
     }
 
-    return Ok(FinalPacket {id: id_data, offset: position + offset, is_need_send: true});
-}
+    frame.offset += position;
 
+    return Ok(FinalPacket {
+        id: id_data,
+        length: length,
+        is_compressed: is_compressed,
+        is_need_send: true,
+    });
+}
 
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind(LISTENER_ADDRESS).await.unwrap();
 
     loop {
+        //
+        //RESET STATES
+        //
+
+        GLOBAL_STATE.store(2, Ordering::Relaxed);
+        IS_COMPRESSED.store(false, Ordering::Relaxed);
+
+        //
+        //WORK WITH SOCKETS
+        //
+
         let (socket, _) = listener.accept().await.unwrap();
         socket.set_nodelay(true).unwrap();
-        let (reader_socket,writer_socket) = socket.into_split();
-    
-        let stream = TcpStream::connect( CONNECTION_ADDRESS).await.unwrap();
+        let (reader_socket, writer_socket) = socket.into_split();
+
+        let stream = TcpStream::connect(CONNECTION_ADDRESS).await.unwrap();
         stream.set_nodelay(true).unwrap();
-    
-        let (reader_stream,writer_stream) = stream.into_split();
-    
+
+        let (reader_stream, writer_stream) = stream.into_split();
+
         //S2C
         tokio::spawn(async move {
-            let mut frame= FramedRead::new(reader_stream, FragmentCodec {});
+            let mut frame = FramedRead::new(reader_stream, FragmentCodec {});
             let mut writer = writer_socket;
             let mut uncompressed_bytes_buffer = BytesMut::zeroed(BUFFER_SIZE);
             uncompressed_bytes_buffer.clear();
 
-            let mut q = 0;
-            let mut time: u128 = 0;
-    
-            while let Some(src) = frame.next().await {
-                match src {
-                    Ok(mut packet_frame) => {
-                        let start = Instant::now();
+            while let Some(Some(mut packet_frame)) = frame.next().await.map(|r| r.ok()) {
+                let compressed_length =
+                    get_compressed_length(&mut packet_frame, IS_COMPRESSED.load(Ordering::Relaxed));
+                let mut val: FinalPacket;
 
-                        let compressed_lenth = unsafe {
-                            get_compressed_lenth(&mut packet_frame)
-                        };
+                if let Some(length) = compressed_length.map(|q| q.get()).filter(|&vl| {
+                    let of = packet_frame.offset;
+                    uncompress(&mut packet_frame, &mut uncompressed_bytes_buffer[of..vl]).is_ok()
+                }) {
+                    val = final_encode(
+                        &mut packet_frame,
+                        Some(&mut uncompressed_bytes_buffer),
+                        length,
+                        true,
+                    )
+                    .unwrap();
+                } else {
+                    let lng = packet_frame.bytes.len();
+                    val = final_encode(&mut packet_frame, None, lng, false).unwrap();
+                }
 
-                        let mut data = &mut packet_frame.bytes;
-                        let offset = packet_frame.offset;
-                        let mut len = data.len();
-                        if let Some(lenth) = compressed_lenth {
-                            let lenth = lenth.get();
-                            if let Ok(_) = uncompress(data,&mut uncompressed_bytes_buffer[offset..lenth], offset) {
-                                data = &mut uncompressed_bytes_buffer;
-                                len = offset + lenth;
-                            }
-                        }
+                let payload: &[u8] = match val.is_compressed {
+                    false => &packet_frame.bytes[packet_frame.offset..val.length],
+                    true => &uncompressed_bytes_buffer[packet_frame.offset..val.length],
+                };
 
-                        unsafe {
-                            let val = final_encode(offset, data, len).unwrap();
-
-                            //offset = val.offset;
-                            let payload: &mut BytesMut = data;
-
-                            match GLOBAL_STATE.load(Ordering::Relaxed) {
-                                //LOGIN
-                                2 => {
-                                    if val.id == 3 {
-                                        IS_COMPRESSED.store(true, Ordering::Relaxed);
-                                    } else if val.id == 2 {
-                                        GLOBAL_STATE.store(3, Ordering::Relaxed);
-                                    }
-                                }
-
-                                //CONFIGURATION
-                                3 => {
-                                    if val.id == 3 {
-                                        GLOBAL_STATE.store(4, Ordering::Relaxed);
-                                    }
-                                }
-
-                                //PLAY
-                                4 => {
-                                    if payload.len() > 1000000 {
-                                        println!("q")
-                                        //Just to ensure accurate speed measurements, so the compiler doesn't optimize away part of the code. 
-                                    }
-                                }
-                                _ => {
-
-                                }
-                            }
-
-                            if val.is_need_send {
-                                let duration = start.elapsed();
-                                q += 1;
-                                time += duration.as_nanos();
-                                if q >= 5000 {
-                                    println!("{0}",time as f64 / 5000.0);
-                                    time = 0;
-                                    q = 0;
-                                }
-                                match writer.write_all_buf(&mut packet_frame.bytes).await {
-                                    Err(e) => {
-                                        println!("{e}");
-                                        break;
-                                    }
-                                    Ok(_) => {}
-                                }
-                            }
+                match GLOBAL_STATE.load(Ordering::Relaxed) {
+                    //LOGIN
+                    2 => {
+                        if val.id == 3 {
+                            IS_COMPRESSED.store(true, Ordering::Relaxed);
+                        } else if val.id == 2 {
+                            GLOBAL_STATE.store(3, Ordering::Relaxed);
                         }
                     }
-                    Err(_) => {
+
+                    //CONFIGURATION
+                    3 => {
+                        if val.id == 3 {
+                            GLOBAL_STATE.store(4, Ordering::Relaxed);
+                        }
+                    }
+
+                    //PLAY
+                    4 => {
+                        //121
+                        let pattern = b"\xc2\xa7n\xc2\xa7o\xc2\xa7m";
+                        if payload
+                            .windows(pattern.len())
+                            .any(|window| window == pattern)
+                        {
+                            val.is_need_send = false;
+                            println!("{},{}", hex::encode_upper(payload), val.id);
+                        };
+
+                        let pattern_fair = b"\xc2\xa7f\xc2\xa7a\xc2\xa7i\xc2\xa7r";
+                        if payload
+                            .windows(pattern_fair.len())
+                            .any(|window| window == pattern_fair)
+                        {
+                            val.is_need_send = false;
+                            println!("{},{}", hex::encode_upper(payload), val.id);
+                        }
+                    }
+                    _ => {}
+                }
+
+                if val.is_need_send {
+                    if let Err(e) = writer.write_all_buf(&mut packet_frame.bytes).await {
+                        println!("{}", e);
                         break;
                     }
                 }
             }
         });
-        
 
         //C2S
         tokio::spawn(async move {
-            let mut frame= FramedRead::new(reader_socket, FragmentCodec {});
+            let mut frame = FramedRead::new(reader_socket, FragmentCodec {});
             let mut writer = writer_stream;
             let mut uncompressed_bytes_buffer = BytesMut::zeroed(BUFFER_SIZE);
             uncompressed_bytes_buffer.clear();
-    
-            while let Some(src) = frame.next().await {
-                match src {
-                    Ok(mut packet_frame) => {
-                        let compressed_lenth = unsafe {
-                            get_compressed_lenth(&mut packet_frame)
-                        };
 
-                        let mut data = &mut packet_frame.bytes;
-                        let offset = packet_frame.offset;
-                        let mut len = data.len();
-                        if let Some(lenth) = compressed_lenth {
-                            let lenth = lenth.get();
-                            if let Ok(_) = uncompress(data,&mut uncompressed_bytes_buffer[offset..lenth],offset) {
-                                data = &mut uncompressed_bytes_buffer;
-                                len = offset + lenth;
-                            }
-                        }
+            while let Some(Some(mut packet_frame)) = frame.next().await.map(|r| r.ok()) {
+                let compressed_length =
+                    get_compressed_length(&mut packet_frame, IS_COMPRESSED.load(Ordering::Relaxed));
+                let mut val: FinalPacket;
 
-                        
-                        unsafe {
-                            let mut val = final_encode(offset, data, len).unwrap();
+                if let Some(length) = compressed_length.map(|q| q.get()).filter(|&vl| {
+                    let of = packet_frame.offset;
+                    uncompress(&mut packet_frame, &mut uncompressed_bytes_buffer[of..vl]).is_ok()
+                }) {
+                    val = final_encode(
+                        &mut packet_frame,
+                        Some(&mut uncompressed_bytes_buffer),
+                        length,
+                        true,
+                    )
+                    .unwrap();
+                } else {
+                    let lng = packet_frame.bytes.len();
+                    val = final_encode(&mut packet_frame, None, lng, false).unwrap();
+                }
 
-                            //offset = val.offset;
-                            let payload: &mut BytesMut = data;
+                let payload: &[u8] = match val.is_compressed {
+                    false => &packet_frame.bytes[packet_frame.offset..val.length],
+                    true => &uncompressed_bytes_buffer[packet_frame.offset..val.length],
+                };
 
-                            match GLOBAL_STATE.load(Ordering::Relaxed) {
-                                //LOGIN
-                                2 => {
-                                    if val.id == 3 {
-                                        IS_COMPRESSED.store(true, Ordering::Relaxed);
-                                    } else if val.id == 2 {
-                                        GLOBAL_STATE.store(3, Ordering::Relaxed);
-                                    }
-                                }
-
-                                //CONFIGURATION
-                                3 => {
-                                    if val.id == 3 {
-                                        GLOBAL_STATE.store(4, Ordering::Relaxed);
-                                    }
-                                }
-
-                                //PLAY
-                                4 => {
-                                    let pattern = b"\xc2\xa7n\xc2\xa7o\xc2\xa7m";
-                                    if payload.windows(pattern.len()).any(|window| window == pattern) {
-                                        val.is_need_send = false;
-                                    };
-
-
-                                    let pattern_fair = b"\xc2\xa7f\xc2\xa7a\xc2\xa7i\xc2\xa7r";
-                                    if payload.windows(pattern_fair.len()).any(|window| window == pattern_fair) {
-                                        val.is_need_send = false;
-                                    }
-                                }
-                                _ => {
-
-                                }
-                            }
-
-                            if val.is_need_send {
-                                match writer.write_all_buf(&mut packet_frame.bytes).await {
-                                    Err(e) => {
-                                        println!("{e}");
-                                        break;
-                                    }
-                                    Ok(_) => {}
-                                }
-                            }
+                match GLOBAL_STATE.load(Ordering::Relaxed) {
+                    //LOGIN
+                    2 => {
+                        if val.id == 3 {
+                            IS_COMPRESSED.store(true, Ordering::Relaxed);
+                        } else if val.id == 2 {
+                            GLOBAL_STATE.store(3, Ordering::Relaxed);
                         }
                     }
-                    Err(_) => {
+
+                    //CONFIGURATION
+                    3 => {
+                        if val.id == 3 {
+                            GLOBAL_STATE.store(4, Ordering::Relaxed);
+                        }
+                    }
+
+                    //PLAY
+                    4 => {
+                        //121
+                        let pattern = b"\xc2\xa7n\xc2\xa7o\xc2\xa7m";
+                        if payload
+                            .windows(pattern.len())
+                            .any(|window| window == pattern)
+                        {
+                            val.is_need_send = false;
+                            println!("{},{}", hex::encode_upper(payload), val.id);
+                        };
+
+                        let pattern_fair = b"\xc2\xa7f\xc2\xa7a\xc2\xa7i\xc2\xa7r";
+                        if payload
+                            .windows(pattern_fair.len())
+                            .any(|window| window == pattern_fair)
+                        {
+                            val.is_need_send = false;
+                            println!("{},{}", hex::encode_upper(payload), val.id);
+                        }
+                    }
+                    _ => {}
+                }
+
+                if val.is_need_send {
+                    if let Err(e) = writer.write_all_buf(&mut packet_frame.bytes).await {
+                        println!("{}", e);
                         break;
                     }
                 }
